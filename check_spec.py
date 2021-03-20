3dc988ed92464624c74ea0c5065795469a23fc98
Wachira Duangdee <wcrcorp@gmail.com>
2021-03-20 15:44:57 +0700

    Create check_spec.py

---
+++ check_spec.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import platform
+
+my_system = platform.uname()
+
+print(f"System: {my_system.system}")
+print(f"Node Name: {my_system.node}")
+print(f"Release: {my_system.release}")
+print(f"Version: {my_system.version}")
+print(f"Machine: {my_system.machine}")
+print(f"Processor: {my_system.processor}")
+
+'''
+
+import wmi
+
+c = wmi.WMI()    
+my_system = c.Win32_ComputerSystem()[0]
+
+print(f"Manufacturer: {my_system.Manufacturer}")
+print(f"Model: {my_system. Model}")
+print(f"Name: {my_system.Name}")
+print(f"NumberOfProcessors: {my_system.NumberOfProcessors}")
+print(f"SystemType: {my_system.SystemType}")
+print(f"SystemFamily: {my_system.SystemFamily}")
+'''
